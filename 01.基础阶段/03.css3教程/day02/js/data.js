22b3c4276614e7aa5c009e399f664bffa83517ea
lumengxin <2735146963@qq.com>
2020-04-09 20:47:56 +0800

    css, h5等基础部分

---
+++ 01.基础阶段/03.css3教程/day02/js/data.js
@@ -0,0 +1,18 @@
+var imgData = {
+	logo:[
+		"img/load/logo2.png",
+		"img/load/logo3.png",
+		"img/load/logo4.png"
+	],
+	logoIco:[
+		"img/load/loadIco1.png",
+		"img/load/loadIco2.png",
+		"img/load/loadIco3.png",
+		"img/load/loadIco4.png",
+		"img/load/loadIco5.png",
+		"img/load/loadIco6.png",
+		"img/load/loadIco7.png",
+		"img/load/loadIco8.png",
+		"img/load/loadIco9.png"
+	]
+};
