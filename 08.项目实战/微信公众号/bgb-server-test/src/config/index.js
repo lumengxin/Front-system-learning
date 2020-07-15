7c53e27477e39f3449e6ea3ad3b28a4aecfcbf79
lumengxin <39377926+lumengxin@users.noreply.github.com>
2020-07-15 15:52:43 +0800

    Update index.js
    隐藏配置

--- 08.项目实战/微信公众号/bgb-server-test/src/config/index.js
+++ 08.项目实战/微信公众号/bgb-server-test/src/config/index.js
@@ -1,8 +1,8 @@
 /* 配置模块 */
 module.exports = {
-  token: 'bbbdyTEST1234',
-  appID: 'wx92721392890c1c33',
-  appsecret: '27f2d8b7255660150acae09dff54a5b7',
-  url: 'http://9d99f048d4a1.ngrok.io'
+  token: '',
+  appID: '',
+  appsecret: '',
+  url: ''
 }
 
